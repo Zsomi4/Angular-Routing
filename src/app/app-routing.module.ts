6dae6edd238e1502271562bc073e3ef855968913
Zsomi4 <k72302872172@ktch.hu>
2022-11-09 11:35:04 +0100

    3 útvonal megírása (shop, product, user)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ShopComponent } from './shop/shop.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/product', component:ProductComponent},
+  {path: '/shop', component:ShopComponent},
+  {path: '/user', component:UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
